c83c2f1e5ea50dc599706f4aec5a07165bc01946
Luiz Felipe <luizfelipe.freire@gmail.com>
2023-03-27 21:54:03 -0300

    Olá Mundo PHP

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Titulo da Aba</title>
+</head>
+<body>
+    <style>
+        /* tag style é um dos lugares para codificar css */
+        h1{
+           color: #ffffff; 
+           background-color: black;
+           text-align: center;
+        }
+    </style>
+    <h1 onclick="alert('Olá Mundo JS')">Minha página</h1>
+    <script>
+        document.write("Olá Mundo JS 2")
+    </script>
+    <?php
+    echo "Olá Mundo PHP";
+    ?>
+</body>
+</html>
