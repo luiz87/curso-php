101436143fc61670e7f8785886ef9c8d861d1cfd
Luiz Felipe <luizfelipe.freire@gmail.com>
2023-04-27 19:53:30 -0300

    Resolução exercício 12 Bootstrap Cards

---
+++ exercicios/exercicio-12-bs-card/index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <style>
+        a {
+            float: right;
+        }
+
+        .card {
+            float: left;
+            margin: 10px;
+            width: 300px;
+        }
+
+        h2 {
+            text-align: center;
+        }
+    </style>
+    <div class="container">
+        <h2>Doces da Dona Chiquinha</h2>
+        <div class="card">
+            <img src="https://raw.githubusercontent.com/luiz87/prj-frontend/master/fake-api/img/chocolate-oreo.webp" alt="" class="card-img-top">
+            <div class="card-body">
+                <h4 class="card-title">Chocolate Oreo</h4>
+                <p class="card-text">R$ 45,00<a href="#" class="btn btn-primary">🛒</a></p>
+
+            </div>
+        </div>
+        <div class="card">
+            <img src="https://raw.githubusercontent.com/luiz87/prj-frontend/master/fake-api/img/brigadeiro-limao.webp" alt="" class="card-img-top">
+            <div class="card-body">
+                <h4 class="card-title">Brigadeiro e Limão</h4>
+                <p class="card-text">R$ 45,00<a href="#" class="btn btn-primary">🛒</a></p>
+
+            </div>
+        </div>
+        <div class="card">
+            <img src="https://raw.githubusercontent.com/luiz87/prj-frontend/master/fake-api/img/brigadeiro-maracuja.webp" alt="" class="card-img-top">
+            <div class="card-body">
+                <h4 class="card-title">Brigadeiro e Maracujá</h4>
+                <p class="card-text">R$ 45,00<a href="#" class="btn btn-primary">🛒</a></p>
+
+            </div>
+        </div>
+        <div class="card">
+            <img src="https://raw.githubusercontent.com/luiz87/prj-frontend/master/fake-api/img/nutela.webp" alt="" class="card-img-top">
+            <div class="card-body">
+                <h4 class="card-title">Leite Ninha com Nutela</h4>
+                <p class="card-text">R$ 45,00<a href="#" class="btn btn-primary">🛒</a></p>
+
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
